417255aa69eb2ebec91d7ed23378f42d071a3c54
lele72297 <18904334771@163.com>
2026-08-01 20:09:29 +0800

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,86 @@
+// Deutsch30 数据保存
+
+
+function getDay(){
+
+
+return Number(
+
+localStorage.getItem("day")
+
+)||1;
+
+
+}
+
+
+
+function saveDay(day){
+
+
+localStorage.setItem(
+
+"day",
+
+day
+
+);
+
+
+}
+
+
+
+function getStreak(){
+
+
+return Number(
+
+localStorage.getItem("streak")
+
+)||0;
+
+
+}
+
+
+
+function saveStreak(number){
+
+
+localStorage.setItem(
+
+"streak",
+
+number
+
+);
+
+
+}
+
+
+
+function completeToday(){
+
+
+let day=getDay();
+
+
+let streak=getStreak();
+
+
+
+if(day<30){
+
+saveDay(day+1);
+
+}
+
+
+saveStreak(streak+1);
+
+
+
+}
 
